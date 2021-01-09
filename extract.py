cd3ddf9d07fddaa886e4839731daa7610dfc8998
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-01-08 19:20:01 -0500

    Create extract.py

---
+++ extract.py
@@ -0,0 +1,14 @@
+img2 = input("Image: ")
+#img2 = 'unnamed.jpg'
+img = cv2.cvtColor(cv2.imread(img2), cv2.COLOR_BGR2RGB)
+detector = MTCNN()
+data = detector.detect_faces(img)
+
+bounding_box = set_data()
+point = bounding_box[0]["keypoints"]
+name = img2 + ".txt"
+output = open(name, "a")
+for key in point:
+    points = list(point[key])
+    print(str(points[0]) + " " + str(points[1]), f=output)
+output.close()
